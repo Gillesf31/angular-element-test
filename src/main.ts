8f9843f5dea8249a218eb614e3066ac17214cd48
Gilles Ferrand <gilles.ferrand31@gmail.com>
2023-07-06 08:28:05 -0400

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,20 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {createCustomElement} from "@angular/elements";
+import {createApplication} from "@angular/platform-browser";
+import {PizzaComponent} from "./app/pizza/pizza.component";
+import {TotoComponent} from "./app/toto/toto.component";
 
-import { AppModule } from './app/app.module';
+(async () => {
+  const app = await createApplication({
+    providers: [],
+  });
 
+  const pizzaElement = createCustomElement(PizzaComponent, {
+    injector: app.injector,
+  });
+  customElements.define('app-pizza-element', pizzaElement);
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+  const totoElement = createCustomElement(TotoComponent, {
+    injector: app.injector,
+  });
+  customElements.define('app-toto-element', totoElement);
+})();
